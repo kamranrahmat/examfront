414afecf3415e789728bdfff7cc9c45b9baf1d2c
SAPPHIRECS\Kamran.Rehmatullah <kamranrahmatullah33@gmail.com>
2022-08-02 18:05:42 +0500

    video 32 11 min end

--- src/app/pages/admin/view-quiz-questions/view-quiz-questions.component.ts
+++ src/app/pages/admin/view-quiz-questions/view-quiz-questions.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { QuestionService } from 'src/app/services/question.service';
 import { QuizService } from 'src/app/services/quiz.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-quiz-questions',
@@ -11,13 +13,28 @@ export class ViewQuizQuestionsComponent implements OnInit {
 
   paramqId=undefined;
   paramtitle=null;
-  constructor(private _activeRoute:ActivatedRoute,private _quiz:QuizService) { }
+  questions:any;
+  constructor(private _activeRoute:ActivatedRoute,private _questionService:QuestionService) { }
   
   ngOnInit(): void {
     this.paramqId=this._activeRoute.snapshot.params['id'];
     this.paramtitle=this._activeRoute.snapshot.params['title'];
+    this.loadAllQuestionsOfQuiz(this.paramqId);
   }
 
-  
+  public loadAllQuestionsOfQuiz(quizId:any){
+    this._questionService.getAllQuestionOfQuiz(quizId).subscribe(
+      (data:any)=>{
+        this.questions=data;
+        console.log(this.questions);
+      },
+      (error)=>{
+        Swal.fire('Error in Loading Questions',error,'error');
+        console.log(error);
+      }
+    );
+
+  }
+
 
 }
