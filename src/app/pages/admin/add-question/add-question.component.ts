ba0a54e2370f7b367a29f66b4d36d88f024bd026
SAPPHIRECS\Kamran.Rehmatullah <kamranrahmatullah33@gmail.com>
2022-08-03 18:47:13 +0500

    video 32

---
+++ src/app/pages/admin/add-question/add-question.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-add-question',
+  templateUrl: './add-question.component.html',
+  styleUrls: ['./add-question.component.css']
+})
+export class AddQuestionComponent implements OnInit {
+
+  qId:any=undefined
+  question={
+    quiz:{
+      qId:this.qId
+    },
+    content:'',
+    option1:'',
+    option2:'',
+    option3:'',
+    option4:'',
+    answer:''
+  }
+  constructor(private activatedRoute:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.qId=this.activatedRoute.snapshot.params['qid'];
+    this.question.quiz['qId']=this.qId;
+  }
+
+}
